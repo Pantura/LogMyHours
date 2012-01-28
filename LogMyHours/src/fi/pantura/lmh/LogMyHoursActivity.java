07c71ebe305afd42bd24ec6a4226152212697a3b
Antti <antti.palola@tut.fi>
2012-01-28 15:05:07 +0200

    Eka commit

---
+++ LogMyHours/src/fi/pantura/lmh/LogMyHoursActivity.java
@@ -0,0 +1,13 @@
+package fi.pantura.lmh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogMyHoursActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
